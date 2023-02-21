bf356d3b81750950fb0a5d072516f7c01b1b7ee6
epherum <wassimfekih2@gmail.com>
2023-02-21 21:22:43 +0100

    improved animations

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,16 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { motion, useAnimationControls } from "framer-motion";
 
 export default function PageTransition({ children }: any) {
   const router = useRouter();
+  const [variantOne, VariantOne] = useState(true);
 
   /* Animation settings */
   const ANIMATION_DURATION = 0.3;
   const controls = useAnimationControls();
-  const variants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1 },
+  const variants1 = {
+    hidden: {
+      opacity: 0,
+      x: -20,
+      transition: { duration: 0.3, ease: "easeOut" },
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: { duration: 0.3, ease: "easeOut" },
+    },
+    reset: { opacity: 0, x: -20, transition: { duration: 0 } },
+    exit: {
+      opacity: 0,
+      x: 20,
+      transition: { duration: 0.3, ease: "easeOut" },
+    },
+  };
+  const variants2 = {
+    hidden: { y: 100 },
+    visible: { y: 0 },
   };
   const transition = {
     duration: ANIMATION_DURATION,
@@ -19,7 +38,7 @@ export default function PageTransition({ children }: any) {
   useEffect(() => {
     /* On click, animate exit and push new route */
     const handleLinkClick = async (event: any) => {
-      await controls.start("hidden");
+      await controls.start("exit");
 
       if (event.detail.replace) {
         router.replace(event.detail.href, undefined, {
@@ -34,7 +53,9 @@ export default function PageTransition({ children }: any) {
 
     /* After route change complete, animate enter */
     const handleRouteChangeComplete = () => {
-      controls.start("visible");
+      controls.start("reset").then(() => {
+        controls.start("visible");
+      });
     };
 
     /* Event listeners */
@@ -49,7 +70,7 @@ export default function PageTransition({ children }: any) {
 
   return (
     <motion.div
-      variants={variants}
+      variants={variantOne ? variants1 : variants2}
       initial="visible"
       animate={controls}
       transition={transition}
