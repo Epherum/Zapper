f4350c09366005f5bd42c898a4f8a47785380afe
epherum <wassimfekih2@gmail.com>
2023-03-18 20:48:29 +0100

    tweaking login

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -11,23 +11,16 @@ export default NextAuth({
     CredentialsProvider({
       name: "Credentials",
       async authorize(credentials, req) {
-        // const res = await fetch(`${process.env.NEXTAUTH_URL}/api/user`, {
-        //   method: "POST",
-        //   headers: {
-        //     "Content-Type": "application/json",
-        //   },
+        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/user`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
 
-        //   body: JSON.stringify(credentials.email),
-        // });
+          body: JSON.stringify(credentials.email),
+        });
 
-        // const user = await res.json();
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
-
-        if (user) {
-          return user;
-        } else {
-          return null;
-        }
+        const user = await res.json();
 
         if (res.ok && user) {
           const isValid = await compare(credentials.password, user.password);
