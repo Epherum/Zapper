0b8798bfe6719c8d89034735265306a8b357fb8b
epherum <wassimfekih2@gmail.com>
2023-03-18 20:27:15 +0100

    test

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -4,7 +4,7 @@ import { collection, getDoc, query, where, doc } from "firebase/firestore";
 export default async function handler(req, res) {
   const email = req.body;
 
-  const docRef = doc(db, "users", email);
+  const docRef = doc(db, "users", "wassimfekih3@gmail.com");
   const querySnapshot = await getDoc(docRef);
   const user = querySnapshot.data();
 
